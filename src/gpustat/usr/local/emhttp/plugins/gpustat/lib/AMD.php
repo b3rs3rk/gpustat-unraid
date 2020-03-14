6708a95d86ceff52f09a30841a5ab5c9782ff3f6
b3rs3rk <b3rs3rk@protonmail.com>
2020-03-14 17:34:34 -0400

    Add placeholder Intel and AMD classes

---
+++ src/gpustat/usr/local/emhttp/plugins/gpustat/lib/AMD.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace gpustat\lib;
+
+/**
+ * Class AMD
+ * @package gpustat\lib
+ */
+class AMD extends Main
+{
+    /**
+     * AMD constructor.
+     * @param array $settings
+     */
+    public function __construct(array $settings = [])
+    {
+        parent::__construct($settings);
+    }
+}
